ff50745bdc38430062878ee9b24e875be2c383af
Rosnec <rosnec.dev@gmail.com>
2014-03-20 18:25:13 -0400

    making command line options

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :url "https://github.com/Rosnec/trivial"
   :license {:name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :jar-name "trivial.jar"
   :uberjar-name "trivial-standalone.jar"
   :main trivial.core)
--- src/trivial/core.clj
+++ src/trivial/core.clj
@@ -1,7 +1,58 @@
 (ns trivial.core
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
+            [trivial.client :as client]
+            [trivial.server :as server])
+  (:import [java.net InetAddress])
   (:gen-class))
 
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 8888
+    :parse-fn Integer/parseInt
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-H" "--hostname HOST" "Remote host"
+    :default (InetAddress/getByName "localhost")
+    :default-desc "localhost"
+    :parse-fn InetAddress/getByName]
+   ["-v" nil "Verbose"
+    :id :verbose
+    ; I might be able to leave out :default
+    ; find out once the program works
+    :default false]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["A proxy server/client program using a modified TFTP."
+        ""
+        "Usage: trivial [options] mode"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Modes:"
+        "  server   Run in server mode"
+        "  client   Run in client mode"]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Runs either the server or client"
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]}
+        (parse-opts args cli-options)]
+    (cond
+     (:help options) (exit 0 (usage summary))
+     (not= (count arguments) 1) (exit 1 (usage summary))
+     errors (exit 1 (error-msg errors)))
+    (case (first arguments)
+      "server" (server/start options)
+      "client" (client/start options)
+      (exit 1 (usage summary)))))
