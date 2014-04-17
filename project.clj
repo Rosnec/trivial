4547e46e6e2be9c31791b81e21b14fc6d321c32d
Rosnec <rosnec.dev@gmail.com>
2014-04-17 02:07:59 -0400

    fixing version number

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject trivial "0.2.0-SNAPSHOT"
+(defproject trivial "1.0.0"
   :description "A proxy server implemented using a slightly modified version
-               of TFTP."
+               of TFTP, implementing sliding window."
   :url "https://github.com/Rosnec/trivial"
   :license {:name "GNU General Public License version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.trace "0.7.8"]
                  [org.clojure/tools.cli "0.3.1"]
                  [gloss "0.2.2"]]
   :jar-name "trivial.jar"
